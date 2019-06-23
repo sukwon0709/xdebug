3ddec1dd8b431532cdc0a0aa14db62d4136bf476
Sukwon Oh <sukwon0709@gmail.com>
2019-06-23 18:30:03 -0400

    Updated build script

--- build_concolic.sh
+++ build_concolic.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 PREFIX_PATH=/home/soh/concolic_php
+CONFIG_PATH=/home/soh/git/uc-php/etc/apache2
+STACK_PATH=/home/soh/.stack/programs/x86_64-linux/ghc-8.6.4
 
 ZLOG_PATH=/home/soh/zlog
 ZLOG_CFLAGS="-I${ZLOG_PATH}/include"
@@ -13,13 +15,13 @@ XXHASH_LDFLAGS="-L${XXHASH_PATH}/lib"
 XXHASH_LIBS="-Wl,-rpath,${XXHASH_PATH}/lib -L${XXHASH_PATH}/lib -lxxhash"
 
 UCPHP_PATH=/home/soh/git/uc-php
-UCPHP_CFLAGS="-I${UCPHP_PATH}/includes"
-UCPHP_LDFLAGS="-L${UCPHP_PATH}"
+UCPHP_CFLAGS="-I${UCPHP_PATH}/includes -I${STACK_PATH}/lib/ghc-8.6.4/include"
+UCPHP_LDFLAGS="-L${UCPHP_PATH} -L${STACK_PATH}/lib/ghc-8.6.4/rts"
 UCPHP_LIBS="-L${UCPHP_PATH} -luc_php -Wl,-rpath,/home/soh/git/uc-php"
-UCPHP2_LIBS="-L${UCPHP_PATH} -luc_php2 -Wl,-rpath,/home/soh/git/uc-php"
+HASKELL_LIBS="-L${STACK_PATH}/lib/ghc-8.6.4/rts -lHSrts-ghc8.6.4 -Wl,-rpath,${STACK_PATH}/lib/ghc-8.6.4/rts"
 
 CFLAGS="${HASKELL_CFLAGS} ${ZLOG_CFLAGS} ${XXHASH_CFLAGS} ${UCPHP_CFLAGS} -DDISABLE_STRING_INTERNING -ggdb3 --coverage"
 LDFLAGS="${HASKELL_LDFLAGS} ${ZLOG_LDFLAGS} ${XXHASH_LDFLAGS} ${UCPHP_LDFLAGS} --coverage"
-LIBS="${HASKELL_LIBS} ${ZLOG_LIBS} ${XXHASH_LIBS} ${UCPHP_LIBS} ${UCPHP2_LIBS}"
+LIBS="${ZLOG_LIBS} ${XXHASH_LIBS} ${UCPHP_LIBS} ${HASKELL_LIBS}"
 
-./configure --prefix=${PREFIX_PATH} --enable-xdebug --with-php-config=/home/soh/concolic_php/bin/php-config CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" LIBS="${LIBS}"
+./configure --prefix=${PREFIX_PATH} --with-php-config=${PREFIX_PATH}/bin/php-config CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" LIBS="${LIBS}"
